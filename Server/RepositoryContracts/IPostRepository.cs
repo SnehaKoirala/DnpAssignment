71661be6694c56d87fe75fb7b81012a177a3eeb1
bibek344326-viaSTE <344326@via.dk>
2024-09-09 11:38:16 +0200

    Updated all

--- Server/RepositoryContracts/IPostRepository.cs
+++ Server/RepositoryContracts/IPostRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace RepositoryContracts;
+﻿using Entities;
+
+namespace RepositoryContracts;
 
 public interface IPostRepository
 {
-    
+    Task<Post> AddAsync(Post post);
+    Task UpdateAsync(Post post);
+    Task DeleteAsync(int id);
+    Task<Post> GetSingleAsync(int id);
+    IQueryable<Post> GetMany();
 }
